fbf9314f2384534e27e67b9b6ad9fae019e69e94
jackjavi <jackmtembete@gmail.com>
2025-11-28 08:03:06 +0300

    Testing onComplete callback fn

--- dss-client/app/whop/page.tsx
+++ dss-client/app/whop/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <div className="pt-32">
       <WhopCheckoutEmbed
+        onComplete={(planId, receiptId) => {
+          console.log(planId, receiptId);
+        }}
         fallback={<>loading...</>}
         planId="plan_G9SKfrwE0PAlP"
         theme="light"
