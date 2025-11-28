6e5a9ed6c4874d3fb68c956c8539c5494c4810d2
jackjavi <jackmtembete@gmail.com>
2025-11-28 03:49:16 +0300

    Fix

--- dss-client/lib/whop-sdk.ts
+++ dss-client/lib/whop-sdk.ts
@@ -1,9 +1,6 @@
 import { Whop } from "@whop/sdk";
 
 export const whopsdk = new Whop({
-  appID: process.env.NEXT_PUBLIC_WHOP_APP_ID,
-  apiKey: process.env.WHOP_API_KEY,
-  webhookKey: btoa(
-    "ws_28c2a0ba7b871781f3e69297d36443e0748ba95ca48b627483daf94564bd613a"
-  ),
+  webhookKey:
+    "ws_28c2a0ba7b871781f3e69297d36443e0748ba95ca48b627483daf94564bd613a",
 });
