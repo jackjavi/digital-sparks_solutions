6eb70c3e5b115dcfd41f7bfa302299a8ca3da715
jackjavi <jackmtembete@gmail.com>
2025-11-28 04:09:58 +0300

    Update sdk

--- dss-client/lib/whop-sdk.ts
+++ dss-client/lib/whop-sdk.ts
@@ -1,6 +1,7 @@
 import { Whop } from "@whop/sdk";
 
 export const whopsdk = new Whop({
-  webhookKey:
-    "ws_28c2a0ba7b871781f3e69297d36443e0748ba95ca48b627483daf94564bd613a",
+  appID: process.env.NEXT_PUBLIC_WHOP_APP_ID,
+  apiKey: process.env.WHOP_API_KEY,
+  webhookKey: btoa(process.env.WHOP_WEBHOOK_SECRET || ""),
 });
